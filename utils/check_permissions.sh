1401b3c45b615206c6b3646edbc2e55f0696c95d
Joshua Cottrell <jcottrell@sbts.edu>
2012-10-05 16:17:25 -0400

    utils update make it easier to run from doc root

--- utils/check_permissions.sh
+++ utils/check_permissions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd utils/
 chown -R deploy:www-data ../../missions-tv/
 chmod -R 750 ../../missions-tv/
 
 # Uploads
 chown -R www-data:www-data ../uploads/
 chmod -R 774 ../uploads/
+
+cd ..
